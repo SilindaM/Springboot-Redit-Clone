1930e01a93d62464cbf92d84631cb0709fdfd5d8
silindaM <silindanemdu@gmail.com>
2021-11-14 13:21:45 +0200

    add voteType class

---
+++ src/main/java/com/example/demo/Model/VoteType.java
@@ -0,0 +1,21 @@
+package com.example.demo.Model;
+
+import java.util.Arrays;
+public enum VoteType {
+
+	UPVOTE(1),DOWNVOTE(-1),
+	;
+	private int direction;
+	
+	  public static VoteType lookup(Integer direction) {
+	        return Arrays.stream(VoteType.values())
+	                .filter(value -> value.getDirection().equals(direction))
+	                .findAny()
+	                .orElseThrow(() -> new SpringRedditException("Vote not found"));
+	    }
+
+	    public Integer getDirection() {
+	        return direction;
+	    }
+	
+}
