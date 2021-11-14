f8c7216d673066e6d1eeb9dc44b15730bcf9ae22
silindaM <silindanemdu@gmail.com>
2021-11-14 13:36:03 +0200

    add Notification Email class

--- src/main/java/com/example/demo/Model/NotificationEmail.java
+++ src/main/java/com/example/demo/Model/NotificationEmail.java
@@ -1,5 +1,14 @@
 package com.example.demo.Model;
 
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class NotificationEmail {
 
+	private String subject;
+	private String recipient;
+	private String body;
 }
