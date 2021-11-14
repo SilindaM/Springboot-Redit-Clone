20c3c52cd8acd3084074cd9b120bb7cf09d362df
silindaM <silindanemdu@gmail.com>
2021-11-14 13:34:16 +0200

    add Verification class

--- src/main/java/com/example/demo/Model/VerificationToken.java
+++ src/main/java/com/example/demo/Model/VerificationToken.java
@@ -1,5 +1,29 @@
 package com.example.demo.Model;
 
+import java.time.Instant;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class VerificationToken {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long Id;
+	private String token;
+	@OneToOne(fetch = FetchType.LAZY)
+	private User user;
+	private Instant expiryDate;
 }
